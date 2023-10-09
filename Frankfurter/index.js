6a412ada941377f7e2321aa3f3e0ad9cf5e56ef9
Felipe Telles <felipe.melo.telles@gmail.com>
2023-10-08 21:12:46 -0300

    getting all currencies

--- Frankfurter/index.js
+++ Frankfurter/index.js
@@ -1,26 +1,40 @@
 // Variables declaration
 import axios from "axios";
 
-const convertCurrencyLatest = async (from, to, amount) => {
+const ConvertCurrencyLatest = async (from, to, amount) => {
   const response = await axios.get(`https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`);
   const result = response.data.rates[to];
   return result.toFixed(2);
 };
 
-const convertCurrencyHistorical = async (from, to, amount, date) => {
+const ConvertCurrencyHistorical = async (from, to, amount, date) => {
   const response = await axios.get(`https://api.frankfurter.app/${date}?amount=${amount}&from=${from}&to=${to}`);
   const result = response.data.rates[to];
   return result.toFixed(2);
 };
 
+const GetAllCurrencies = async () => {
+  const response = await axios.get(`https://api.frankfurter.app/currencies`);
+  const currencyCodes = Object.keys(response.data);
+  const currencyNames = Object.values(response.data);
+  return {currencyCodes, currencyNames};
+};
+
 const run = async () => {
-  console.log('Currency Converter');
   const from = 'USD';
   const to = 'BRL';
   const amount = 10;
   const date = '2012-01-05';
-  const convertedAmount = await convertCurrencyLatest(from, to, amount);
-  const convertedAmountHistorical = await convertCurrencyHistorical(from, to, amount, date);
+
+  const {currencyCodes, currencyNames} = await GetAllCurrencies();
+  const convertedAmount = await ConvertCurrencyLatest(from, to, amount);
+  const convertedAmountHistorical = await ConvertCurrencyHistorical(from, to, amount, date);
+  
+  for (let i = 0; i < currencyCodes.length; i++) {
+    const code = currencyCodes[i];
+    const name = currencyNames[i];
+    console.log(`${name} (${code})`);
+  }
   console.log(`Now: ${amount} ${from} = ${convertedAmount} ${to}`);
   console.log(`In ${date}: ${amount} ${from} = ${convertedAmountHistorical} ${to}`);
 };
